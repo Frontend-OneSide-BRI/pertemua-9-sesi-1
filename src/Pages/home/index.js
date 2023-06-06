c0cdb8c9865e1d60d466c9d81aca3417f5a8e563
Azim <budazimbud@gmail.com>
2023-06-06 11:04:55 +0700

    update

--- src/Pages/home/index.js
+++ src/Pages/home/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>home</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      home
+      <button
+        onClick={(event) => {
+          localStorage.removeItem("userData");
+          navigate("/login");
+        }}
+      >
+        logout
+      </button>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,20 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-function Layout({ children }) {
+function Layout({ children, access }) {
   const navigate = useNavigate();
   useEffect(() => {
     if (!localStorage.getItem("userData")) {
       navigate("/login");
     }
   }, [navigate]);
+
+  useEffect(() => {
+    //tambahkan logic authorization hak akses ketika sudah login
+  }, [access]);
   return (
     <div>
+      <h1>{access}</h1>
       <h1>Di tambahkan layout</h1>
       {children}
     </div>
